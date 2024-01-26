4b170437fe2a2e8feae65799df8e9927ad4f1a6c
Vipan Kumar <vipan.webdeveloper@gmail.com>
2024-01-26 15:32:33 +0530

    worked onit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -12,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'book-list-search'`, () => {
+  it(`should have as title 'SafeNavigationOperator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-list-search');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('SafeNavigationOperator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('book-list-search app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('SafeNavigationOperator app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { Book } from '../assets/models/book'
-import { BOOK } from '../assets/models/book-mock';
+
+import {userList} from './user'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'book-list-search';
-  books: Book[] = BOOK;
-  constructor() { }
 
-  ngOnInit(): void {
-    this.books = BOOK;
+export class AppComponent {
+  userList:any = [];
+  constructor(){
+    this.userList = userList;
   }
 }
+
